e05489606b3ed7663aeeceff272352fcf79e51e9
dandy <dandy@github.com>
2024-09-03 08:32:28 +0800

    update

---
+++ 代码随想录一刷 2024.8.1/double_point/doubel_point.go
@@ -0,0 +1,10 @@
+package double_point
+
+// 27. 移除元素
+// 输入：nums = [3,2,2,3], val = 3
+// 输出：2, nums = [2,2,_,_]
+// 解释：你的函数函数应该返回 k = 2, 并且 nums 中的前两个元素均为 2。
+// 你在返回的 k 个元素之外留下了什么并不重要（因此它们并不计入评测）。
+func removeElement(nums []int, val int) int {
+
+}
